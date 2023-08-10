f13cd8abb66286d0ab77144c0b5f1cb8827a1a1d
Phecobaba <mrpheco@gmail.com>
2023-08-10 10:46:06 -0700

    print random figures

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 
-if number < 0 and last_digit != 0:
+
+if number < 0:
     last_digit = -last_digit
+
 
 if last_digit < 6 and last_digit != 0:
     print('last digit of', number, 'is', last_digit,
